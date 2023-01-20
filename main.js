2a7b4520143cd15ebadba7a274021b0869b69a33
masaue <masayuki.uegaki@gmail.com>
2023-01-21 04:40:28 +0900

    port button

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 main();
 
 async function main() {
+  /*
+   * GPIO-Buttonの処理を移植
+   */
+  var gpioAccess = await navigator.requestGPIOAccess();
+  var ledPort = gpioAccess.ports.get(26); // LEDの付いているポート
+  await ledPort.export("out");
+  var switchPort = gpioAccess.ports.get(5); // タクトスイッチの付いているポート
+  await switchPort.export("in");
+  switchPort.onchange = function(val) {
+    // スイッチはPullupで離すと1なので反転させる
+    ledPort.write(val === 0 ? 1 : 0);
+  };
+
   try {
     var temp = document.getElementById("temp");
     var gx = document.getElementById("gx");
